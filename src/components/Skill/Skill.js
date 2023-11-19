86261be4003a27e26913337e1d2f4bc0de8416c2
im-waseem <iamwaseem0066@gmail.com>
2023-11-19 12:19:34 +0530

    Skills changes

--- src/components/Skill/Skill.js
+++ src/components/Skill/Skill.js
@@ -1,64 +1,64 @@
-import React, { useState } from 'react';
-import { FaHtml5, FaCss3, FaJs, FaReact, FaNodeJs, FaDatabase, FaCode, FaToolbox, FaQuestion } from 'react-icons/fa';
-
+// Skill.js
+import React from 'react';
+import { FaHtml5, FaCss3, FaJs, FaReact, FaJava, FaAws, FaPython, FaGithub, FaNodeJs, FaDatabase, FaCode, FaToolbox, FaQuestion , FaQuestionCircle} from 'react-icons/fa';
+import { Card } from 'react-bootstrap';
 import './Skill.css';
 
 const Skill = () => {
-  const [selectedCategory, setSelectedCategory] = useState('frontend'); // Default to Frontend Skills
-
   const frontendSkills = [
-    { icon: <FaHtml5 />, name: 'HTML' },
-    { icon: <FaCss3 />, name: 'CSS' },
-    { icon: <FaJs />, name: 'JavaScript' },
-    { icon: <FaReact />, name: 'React' },
-    { icon: <FaCode />, name: 'Bootstrap' }, // Use a generic code icon for Bootstrap
-    { icon: <FaCode />, name: 'jQuery' },   // Use a generic code icon for jQuery
+    { icon: <FaHtml5 color="#E44D26" size={120} />, name: 'HTML' },
+    { icon: <FaCss3 color="#1572B6" size={120} />, name: 'CSS' },
+    { icon: <FaJs color="#F0DB4F" size={120} />, name: 'JavaScript' },
+    { icon: <FaReact color="#61DAFB" size={120} />, name: 'React' },
+    { icon: <FaCode color="black" size={120} />, name: 'Bootstrap' },
+    { icon: <FaQuestionCircle color="orange" size={120} />, name: 'jQuery' },
+    // Add more frontend skills here
   ];
   const backendSkills = [
-    { icon: <FaNodeJs />, name: 'Node.js' },
-    { icon: <FaDatabase />, name: 'MongoDB' },
+    { icon: <FaNodeJs color="#68A063" size={120} />, name: 'Node.js' },
+    { icon: <FaDatabase color="#47A248" size={120} />, name: 'MongoDB' },
+    { icon: <FaDatabase color="#007ACC" size={120} />, name: 'SQL' }, // Using the database icon for SQL
+
+
     // Add more backend skills here
   ];
 
   const tools = [
-    { icon: <FaCode />, name: 'VS Code' },
-    { icon: <FaToolbox />, name: 'Postman' },
-    { icon: <FaReact />, name: 'Heroku' },
+    { icon: <FaCode color="#007ACC" size={120} />, name: 'VS Code' },
+    { icon: <FaToolbox color="#FF6C37" size={120} />, name: 'Postman' },
+    { icon: <FaReact color="#61DAFB" size={120} />, name: 'Heroku' },
+    { icon: <FaGithub color="#000" size={120} />, name: 'GitHub' },
+    { icon: <FaDatabase color="#47A248" size={120} />, name: 'MongoDB' },
+
     // Add more tools here
   ];
 
-  const upcomingSkills = [
-    { icon: <FaQuestion />, name: 'DevOps' },
-    { icon: <FaQuestion />, name: 'Cyber Security' },
-    { icon: <FaQuestion />, name: 'Hacking' },
-    // Add more upcoming skills here
+  const learningSkills = [
+    { icon: <FaAws color="#FF9900" size={120} />, name: 'AWS' },
+    { icon: <FaPython color="#3776AB" size={120} />, name: 'Python' },
+    { icon: <FaJava color="#5382A1" size={120} />, name: 'Java' },
+    { icon: <FaNodeJs color="#68A063" size={120} />, name: 'Django' }, // Assuming Django represents your backend
+    // Add more learning skills here
   ];
 
-  const getSkillsByCategory = () => {
-    switch (selectedCategory) {
-      case 'frontend':
-        return frontendSkills;
-      case 'backend':
-        return backendSkills;
-      case 'tools':
-        return tools;
-      case 'upcoming':
-        return upcomingSkills;
-      default:
-        return [];
-    }
-  };
+
+  const upcomingSkills = [
+    { icon: <FaQuestion color="grey" size={120} />, name: 'DevOps' },
+    { icon: <FaQuestion color="grey" size={120} />, name: 'Cyber Security' },
+    { icon: <FaQuestion color="grey" size={120} />, name: 'Hacking' },
+    // Add more upcoming skills here
+  ];
 
   const renderSkills = (skills) => {
     return (
       <div className="skills-container">
         {skills.map((skill, index) => (
-          <div className="skill-item" key={index} title={`I am ${skill.name}`}>
-            <span className="skill-icon">
-              {skill.icon}
-            </span>
-            <span className="skill-name">{skill.name}</span>
-          </div>
+          <Card key={index} className="skill-card">
+            <Card.Body>
+              <div className="skill-icon">{skill.icon}</div>
+              <Card.Title className="skill-name">{skill.name}</Card.Title>
+            </Card.Body>
+          </Card>
         ))}
       </div>
     );
@@ -66,13 +66,48 @@ const Skill = () => {
 
   return (
     <div className="skill-wrapper">
-      <div className="category-buttons">
-        <button onClick={() => setSelectedCategory('frontend')}>Frontend Skills</button>
-        <button onClick={() => setSelectedCategory('backend')}>Backend Skills</button>
-        <button onClick={() => setSelectedCategory('tools')}>Tools</button>
-        <button onClick={() => setSelectedCategory('upcoming')}>Upcoming Skills</button>
+      <div className="category-card">
+        <Card.Header>
+          <h2 className="category-title">Frontend Skills</h2>
+        </Card.Header>
+        <div className="category-content">
+          {renderSkills(frontendSkills)}
+        </div>
       </div>
-      {renderSkills(getSkillsByCategory())}
+
+      <div className="category-card">
+        <Card.Header>
+          <h2 className="category-title">Backend Skills</h2>
+        </Card.Header>
+        <div className="category-content">
+          {renderSkills(backendSkills)}
+        </div>
+      </div>
+
+      <div className="category-card">
+        <Card.Header>
+          <h2 className="category-title">Tools</h2>
+        </Card.Header>
+        <div className="category-content">
+          {renderSkills(tools)}
+        </div>
+      </div>
+      <div className="category-card">
+        <Card.Header>
+          <h2 className="category-title">Learning Skills</h2>
+        </Card.Header>
+        <div className="category-content">
+          {renderSkills(learningSkills)}
+        </div>
+      </div>
+      <div className="category-card">
+        <Card.Header>
+          <h2 className="category-title">Upcoming Skills</h2>
+        </Card.Header>
+        <div className="category-content">
+          {renderSkills(upcomingSkills)}
+        </div>
+      </div>
     </div>
   );
 };
